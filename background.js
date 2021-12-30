8b461c5ffb91d4e6a76f2d13e4a509c4fcac01ba
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-30 09:13:07 +0100

    Add function for translation api call

--- background.js
+++ background.js
@@ -9,25 +9,42 @@ chrome.runtime.onInstalled.addListener(function() {
 
 var storage = chrome.storage.local;
 
-/*
+
 storage.remove("words", function (){
            console.log("Words has been removed");
-       });
-*/
+});
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	storage.get("words", function (items){        
-    	console.log(items)
 		if(items.words != undefined) {
+			console.log("Got word: " + info.selectionText);
+			translate(info.selectionText);
 	   		var updated_list = items.words.slice();
 			updated_list.push(info.selectionText)
-      		storage.set({"words":updated_list}, function (){
-           		console.log("Words has been updated");
+			storage.set({"words":updated_list}, function (){
+           	console.log("Words has been updated");
        		});
     	}
     	else {
-        	storage.set({"words":[info.selectionText]}, function (){
-            	console.log("Words has been initiated");
+			console.log("Got word: " + info.selectionText);
+			translate(info.selectionText);
+			storage.set({"words":[info.selectionText]}, function (){
+            console.log("Words has been initiated");
         	});
    		 }
 	});
 })
+
+function translate(word) {
+	fetch("https://api-free.deepl.com/v2/translate", {
+  		body: "auth_key=d0477023-8452-ae5a-f7ba-bd904a015b6a:fx&text=" + word + "&target_lang=ES",
+ 		headers: {
+   			 "Content-Type": "application/x-www-form-urlencoded"
+ 		},
+  		method: "POST"
+	})
+	.then(response => response.json())
+  	.then(data => console.log("Word translated to: " + data.translations[0].text));
+}
+
+
