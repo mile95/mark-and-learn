97a700df8c1b991997655f10f21829a784212db7
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-31 00:33:06 +0100

    Formatted js code

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
         "id": "myContextMenuId"
     });
 });
-    
+
 
 var storage = chrome.storage.local;
 
@@ -15,36 +15,40 @@ var storage = chrome.storage.local;
 //});
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-		translateAndStoreWord(info.selectionText);
+    translateAndStoreWord(info.selectionText);
 })
 
 function translateAndStoreWord(word) {
-	fetch("https://api-free.deepl.com/v2/translate", {
-  		body: "auth_key=d0477023-8452-ae5a-f7ba-bd904a015b6a:fx&text=" + word + "&target_lang=ES",
- 		headers: {
-   			 "Content-Type": "application/x-www-form-urlencoded"
- 		},
-  		method: "POST"
-	})
-	.then(response => response.json())
-  	.then(data => storeWordAndTranslation(word,data.translations[0].text));
+    fetch("https://api-free.deepl.com/v2/translate", {
+            body: "auth_key=d0477023-8452-ae5a-f7ba-bd904a015b6a:fx&text=" + word + "&target_lang=ES",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST"
+        })
+        .then(response => response.json())
+        .then(data => storeWordAndTranslation(word, data.translations[0].text));
 }
 
 function storeWordAndTranslation(word, translation) {
-	storage.get("words", function (items) {
-		if(items.words != undefined) {
-			let old_words = items.words;
-			old_words[word] = translation
-			console.log(old_words);
-			storage.set({"words": old_words}, function () {
-				console.log("Words has been updated")
-			});
-		} else {
-			let words = {};
-			words[word] = translation
-			storage.set({"words":words}, function () {
-				console.log("Words has been initiated");
-			});
-		}
-	});
+    storage.get("words", function(items) {
+        if (items.words != undefined) {
+            let old_words = items.words;
+            old_words[word] = translation
+            console.log(old_words);
+            storage.set({
+                "words": old_words
+            }, function() {
+                console.log("Words has been updated")
+            });
+        } else {
+            let words = {};
+            words[word] = translation
+            storage.set({
+                "words": words
+            }, function() {
+                console.log("Words has been initiated");
+            });
+        }
+    });
 }
--- popup.js
+++ popup.js
@@ -1,18 +1,19 @@
-chrome.storage.local.get("words", ({ words }) => {
-  tableCreate(words);
+chrome.storage.local.get("words", ({
+    words
+}) => {
+    tableCreate(words);
 });
 
 function tableCreate(words) {
-  const body = document.body
-  const tbl = document.createElement('table');
-  Object.keys(words)
-		.forEach(function eachKek(key) {
-			const tr = tbl.insertRow();
-			const tdFrom = tr.insertCell();
-			const tdTo = tr.insertCell();
-			tdFrom.appendChild(document.createTextNode(key));
-			tdTo.appendChild(document.createTextNode(words[key]));
-		});
-  body.appendChild(tbl)
+    const body = document.body
+    const tbl = document.createElement('table');
+    Object.keys(words)
+        .forEach(function eachKek(key) {
+            const tr = tbl.insertRow();
+            const tdFrom = tr.insertCell();
+            const tdTo = tr.insertCell();
+            tdFrom.appendChild(document.createTextNode(key));
+            tdTo.appendChild(document.createTextNode(words[key]));
+        });
+    body.appendChild(tbl)
 }
-
