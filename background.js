3cca9302c43fb1d3285f1bf0a8193453c7968846
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-31 00:28:34 +0100

    Save words like key:value object

--- background.js
+++ background.js
@@ -10,32 +10,15 @@ chrome.runtime.onInstalled.addListener(function() {
 var storage = chrome.storage.local;
 
 
-storage.remove("words", function (){
-           console.log("Words has been removed");
-});
+//storage.remove("words", function (){
+//           console.log("Words has been removed");
+//});
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	storage.get("words", function (items){        
-		if(items.words != undefined) {
-			console.log("Got word: " + info.selectionText);
-			translate(info.selectionText);
-	   		var updated_list = items.words.slice();
-			updated_list.push(info.selectionText)
-			storage.set({"words":updated_list}, function (){
-           	console.log("Words has been updated");
-       		});
-    	}
-    	else {
-			console.log("Got word: " + info.selectionText);
-			translate(info.selectionText);
-			storage.set({"words":[info.selectionText]}, function (){
-            console.log("Words has been initiated");
-        	});
-   		 }
-	});
+		translateAndStoreWord(info.selectionText);
 })
 
-function translate(word) {
+function translateAndStoreWord(word) {
 	fetch("https://api-free.deepl.com/v2/translate", {
   		body: "auth_key=d0477023-8452-ae5a-f7ba-bd904a015b6a:fx&text=" + word + "&target_lang=ES",
  		headers: {
@@ -44,7 +27,24 @@ function translate(word) {
   		method: "POST"
 	})
 	.then(response => response.json())
-  	.then(data => console.log("Word translated to: " + data.translations[0].text));
+  	.then(data => storeWordAndTranslation(word,data.translations[0].text));
 }
 
-
+function storeWordAndTranslation(word, translation) {
+	storage.get("words", function (items) {
+		if(items.words != undefined) {
+			let old_words = items.words;
+			old_words[word] = translation
+			console.log(old_words);
+			storage.set({"words": old_words}, function () {
+				console.log("Words has been updated")
+			});
+		} else {
+			let words = {};
+			words[word] = translation
+			storage.set({"words":words}, function () {
+				console.log("Words has been initiated");
+			});
+		}
+	});
+}
--- popup.js
+++ popup.js
@@ -3,13 +3,16 @@ chrome.storage.local.get("words", ({ words }) => {
 });
 
 function tableCreate(words) {
-  const body = document.body,
-        tbl = document.createElement('table');
-  for (var i = 0; i < words.length; i++) {
-	const tr = tbl.insertRow();
-	const td = tr.insertCell();
-	td.appendChild(document.createTextNode(words[i]));
-  }
+  const body = document.body
+  const tbl = document.createElement('table');
+  Object.keys(words)
+		.forEach(function eachKek(key) {
+			const tr = tbl.insertRow();
+			const tdFrom = tr.insertCell();
+			const tdTo = tr.insertCell();
+			tdFrom.appendChild(document.createTextNode(key));
+			tdTo.appendChild(document.createTextNode(words[key]));
+		});
   body.appendChild(tbl)
 }
 
