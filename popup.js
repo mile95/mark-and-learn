a94c4b0337f5fd47cd979af27cc18c3c0d2da4a8
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-31 01:06:09 +0100

    Minor css improvements

--- popup.js
+++ popup.js
@@ -7,6 +7,14 @@ chrome.storage.local.get("words", ({
 function tableCreate(words) {
     const body = document.body
     const tbl = document.createElement('table');
+    tbl.id = "words";
+
+    const header = tbl.createTHead();
+    const headerRow = header.insertRow(0);
+    const headerCellFrom = headerRow.insertCell(0);
+    const headerCellTo = headerRow.insertCell(1);
+    headerCellFrom.appendChild(document.createTextNode("English"));
+    headerCellTo.appendChild(document.createTextNode("Spanish"));
     Object.keys(words)
         .forEach(function eachKek(key) {
             const tr = tbl.insertRow();
