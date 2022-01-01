50a64a52500f149214ed33643a16bce5845d3205
Fredrik Mile <fredrik.mile@gmail.com>
2022-01-01 21:55:51 +0100

    Improve table css

--- popup.js
+++ popup.js
@@ -7,21 +7,24 @@ chrome.storage.local.get("words", ({
 function tableCreate(words) {
     const body = document.body
     const tbl = document.createElement('table');
-    tbl.id = "words";
 
-    const header = tbl.createTHead();
-    const headerRow = header.insertRow(0);
-    const headerCellFrom = headerRow.insertCell(0);
-    const headerCellTo = headerRow.insertCell(1);
-    headerCellFrom.appendChild(document.createTextNode("English"));
-    headerCellTo.appendChild(document.createTextNode("Spanish"));
+    var thead = document.createElement('thead');
+    tbl.appendChild(thead);
+    thead.appendChild(document.createElement("th")).
+    appendChild(document.createTextNode("English"));
+    thead.appendChild(document.createElement("th")).
+    appendChild(document.createTextNode("Spanish"));
     Object.keys(words)
         .forEach(function eachKek(key) {
             const tr = tbl.insertRow();
             const tdFrom = tr.insertCell();
             const tdTo = tr.insertCell();
-            tdFrom.appendChild(document.createTextNode(key));
-            tdTo.appendChild(document.createTextNode(words[key]));
+            tdFrom.appendChild(document.createTextNode(capitalizeFirstLetter(key)));
+            tdTo.appendChild(document.createTextNode(capitalizeFirstLetter(words[key])));
         });
     body.appendChild(tbl)
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
